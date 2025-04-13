830fc67a91d36a78adb7a7bcdaa4c40af9553c6e
jon-snow76 <vaishnavk076@gmail.com>
2025-04-13 22:12:41 +0530

    Configuring jdbc

---
+++ src/Demojdbc.java
@@ -0,0 +1,17 @@
+
+import java.sql.*;
+public class Demojdbc {
+
+    /*
+    * import package
+    * load and register
+    * create connection
+    * create statement
+    * execute statement
+    * process the results
+    * close
+    * */
+    Class.forName("org.postgree.sql");
+
+
+}
