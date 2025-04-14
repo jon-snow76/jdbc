34b8434d74c1db3e357e1c3e48c281b3abbbd2af
jon-snow76 <vaishnavk076@gmail.com>
2025-04-14 15:00:07 +0530

    Configuring jdbc 2.0

--- src/Demojdbc.java
+++ src/Demojdbc.java
@@ -1,17 +1,53 @@
 
 import java.sql.*;
 public class Demojdbc {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-    /*
-    * import package
-    * load and register
-    * create connection
-    * create statement
-    * execute statement
-    * process the results
-    * close
-    * */
-    Class.forName("org.postgree.sql");
+        /*
+         * import package
+         * load and register
+         * create connection
+         * create statement
+         * execute statement
+         * process the results
+         * close
+         * */
+        int sid =6;
+        int marks =100;
+        String sname = "krisvais";
+        String url ="jdbc:postgresql://localhost:5432/Demo";
+        String uname= "postgres";
+        String pass = "****";
+        String sql = "Insert into Student values (?,?,?)";
+//        Class.forName("org.postgresql.Driver");
+
+        //Establishing connnection
+        Connection con = DriverManager.getConnection(url,uname,pass);
+//        System.out.println("Connection Established");
+//        Statement st = con.createStatement();
+
+        /*
+        * Prepared statement performs functionality of a cache memory thus helping to
+        * reduce the amout of reads on a DB*/
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1,sid);
+        ps.setInt(2,marks);
+        ps.setString(3,sname);
+        ps.execute();
+//        boolean res= st.execute(sql);
+//        ResultSet rs = st.executeQuery(sql);
+        //Moves the result pointer by one, therefore giving the right result
+//        while(rs.next()){
+//            System.out.print(rs.getInt(1) + " - ");
+//            System.out.print(rs.getInt(2) + " - ");
+//            System.out.println(rs.getString(3));
+//        }
+//        System.out.println(res);
+        con.close();
+        System.out.println("Connection Closed");
+    }
+
+
 
 
 }
